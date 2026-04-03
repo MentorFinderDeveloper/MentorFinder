e390a5bb0f38e02ab86a925b97ee7570f92934ed
4671 <18201241302@163.com>
2026-04-04 01:05:29 +0800

    refactor: change DjangoHW to MFBackend #5

--- DjangoHW/asgi.py
+++ DjangoHW/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for DjangoHW project.
+ASGI config for MFBackend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoHW.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MFBackend.settings')
 
 application = get_asgi_application()
--- DjangoHW/wsgi.py
+++ DjangoHW/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DjangoHW project.
+WSGI config for MFBackend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoHW.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MFBackend.settings')
 
 application = get_wsgi_application()
