2f4e68a95d9b27b82ccaf915c5e72566eb952110
4671 <18201241302@163.com>
2026-04-07 00:27:44 +0800

    fix: changed ci and start.sh #18

--- start.sh
+++ start.sh
@@ -2,11 +2,8 @@
 python3 manage.py makemigrations account
 python3 manage.py makemigrations dataset
 python3 manage.py migrate
-python3 manage.py createsuperuser --noinput
-
-uwsgi --module=MFBackend.wsgi:application \
-    --env DJANGO_SETTINGS_MODULE=MFBackend.settings \
-    --master \
+python3 manage.py createsuperuser --noinput || true
+-master \
     --http=0.0.0.0:80 \
     --processes=5 \
     --harakiri=20 \
