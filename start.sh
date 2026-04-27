868c43b73aff11de39a399e8fa47a7fd5602f3ac
4671 <18201241302@163.com>
2026-04-27 09:37:13 +0800

    fix: 502 Empty response body #36

--- start.sh
+++ start.sh
@@ -11,10 +11,19 @@ fi
 
 python3 manage.py migrate --noinput
 python3 manage.py createsuperuser --noinput || true
-python3 manage.py sync_dataset || true
-python3 manage.py run_daily_sync &
 
-uwsgi --module=MFBackend.wsgi:application \
+# Avoid startup 502 caused by long-running crawler tasks blocking uwsgi boot.
+# Set SYNC_DATA_ON_STARTUP=1 if you need a one-time sync after container starts.
+if [ "${SYNC_DATA_ON_STARTUP:-0}" = "1" ]; then
+    (python3 manage.py sync_dataset || true) &
+fi
+
+# Daily scheduler can be disabled by setting RUN_DAILY_SYNC_SCHEDULER=0.
+if [ "${RUN_DAILY_SYNC_SCHEDULER:-1}" = "1" ]; then
+    python3 manage.py run_daily_sync &
+fi
+
+exec uwsgi --module=MFBackend.wsgi:application \
     --env DJANGO_SETTINGS_MODULE=MFBackend.settings \
     --master \
     --http=0.0.0.0:80 \
