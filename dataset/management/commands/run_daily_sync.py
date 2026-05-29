42f0f5bd929cd03e47bbe63f959eb58ed846dce0
wangjinbo2006 <qd_wangjinbo@163.com>
2026-05-30 03:52:04 +0800

    Restore daily sync schedule to 04:00

--- dataset/management/commands/run_daily_sync.py
+++ dataset/management/commands/run_daily_sync.py
@@ -23,11 +23,11 @@ def run_weekly_push_job():
 
 
 class Command(BaseCommand):
-    help = "每天定时执行 sync_dataset，默认在 Asia/Shanghai 时区 03:50 运行"
+    help = "每天定时执行 sync_dataset，默认在 Asia/Shanghai 时区 04:00 运行"
 
     def add_arguments(self, parser):
-        parser.add_argument("--hour", type=int, default=3, help="每日执行小时，默认 3")
-        parser.add_argument("--minute", type=int, default=50, help="每日执行分钟，默认 50")
+        parser.add_argument("--hour", type=int, default=4, help="每日执行小时，默认 4")
+        parser.add_argument("--minute", type=int, default=0, help="每日执行分钟，默认 0")
 
     def handle(self, *args, **options):
         from apscheduler.schedulers.blocking import BlockingScheduler
