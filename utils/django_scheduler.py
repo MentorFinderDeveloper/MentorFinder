c254b18e5eaf7152616974f2c306105cb86163fa
syj <syj24@mails.tsinghua.edu.cn>
2026-05-31 18:16:23 +0800

    test: crawl at 18:17 #58

--- utils/django_scheduler.py
+++ utils/django_scheduler.py
@@ -53,7 +53,7 @@ def start_django_scheduler() -> bool:
         if config["run_daily_sync_scheduler"]:
             scheduler.add_job(
                 _run_sync_dataset_job,
-                trigger=CronTrigger(hour=18, minute=6, timezone=timezone),
+                trigger=CronTrigger(hour=18, minute=17, timezone=timezone),
                 id="daily_sync_dataset",
                 replace_existing=True,
                 coalesce=True,
